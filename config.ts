364383e4afd91a0beb9ba1e7270c12cb0de9879d
bigpapamamahouse <grantswingler@gmail.com>
2025-11-09 15:41:48 -0600

    Delete config.ts

--- config.ts
+++
@@ -1,5 +0,0 @@
-export const CONFIG = {
-  apiUrl: import.meta.env.VITE_API_URL || "",
-  userPoolId: import.meta.env.VITE_USER_POOL_ID || "",
-  userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID || ""
-};
